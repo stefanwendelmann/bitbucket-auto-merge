b9b34d0c835ea2e0bb67a1727dc0dad6eaca8d7a
Stefan <steveoh444@gmail.com>
2023-08-07 16:51:05 +0200

    Update auto-merge.sh
    - Add Rate Limit for PR creation with default -1 (off).
    - Check if PR exist by title and status OPEN before create a new PR

--- auto-merge.sh
+++ auto-merge.sh
@@ -1,71 +1,101 @@
+#!/bin/bash
+
+PR_RUN_LIMIT=${AUTO_MERGE_PR_RUN_LIMIT=-1}
+PR_COUNT=0
+
 echo "Current Branch is: $BITBUCKET_BRANCH"
 
-git checkout $BITBUCKET_BRANCH #>/dev/null 2>&1
+if [ $PR_RUN_LIMIT -lt 0 ]; then
+  echo "WARNING: No PR Rate Limit set... to set it, set the AUTO_MERGE_PR_RUN_LIMIT in the workspace or repository variables."
+else
+  echo "PR Rate Limit is: $PR_RUN_LIMIT"
+  if [ $PR_RUN_LIMIT -eq 0 ]; then
+    echo "WARNING: Rate Limit of 0 means no PR's will be created !"
+  fi
+fi
+
+###############
+
+git checkout $BITBUCKET_BRANCH                                                                                       #>/dev/null 2>&1
 git branch -r | grep -v '\->' | while read remote; do git branch --track "${remote#origin/}" "$remote" || true; done #>/dev/null 2>&1 #set tracking of all remote branches
-git fetch --all #>/dev/null 2>&1 # fetch all remote branches to the local repository
-git pull --all #>/dev/null 2>&1 # update all local branches
-for BRANCH in `git branch --list | sed 's/\*//g'`
-do
-    COMMITS_AHEAD_OF_MASTER=`git log $BITBUCKET_BRANCH..$BRANCH`
-    if [ -n "$COMMITS_AHEAD_OF_MASTER" ]
-    then
-        #echo "I am Ahead of my $BITBUCKET_BRANCH: $BRANCH ... now check behind"
-        COMMITS_BEHIND_MASTER=`git log $BRANCH..$BITBUCKET_BRANCH`
-        if [ -n "$COMMITS_BEHIND_MASTER" ]
-        then
-            echo "Try to merge $BITBUCKET_BRANCH into $BRANCH"
-            git checkout $BRANCH
-            git merge --no-commit --no-ff $BITBUCKET_BRANCH
-            CONFLICTS=$(git ls-files -u | wc -l)
-            if [ "$CONFLICTS" -gt 0 ] ; then
-               echo "There is a merge conflict. Aborting..."
-               git merge --abort
-               # in pipeline Sicherstellen das curl und jq installiert sind
-               # apt-get update
-               # apt-get -y install curl jq
-               export BB_TOKEN=$(curl -s -S -f -X POST -u "${BB_AUTH_STRING}" \
-                https://bitbucket.org/site/oauth2/access_token \
-                -d grant_type=client_credentials -d scopes="repository" | jq --raw-output '.access_token')
-               #echo "$BB_TOKEN"
+git fetch --all                                                                                                      #>/dev/null 2>&1 # fetch all remote branches to the local repository
+git pull --all                                                                                                       #>/dev/null 2>&1 # update all local branches
+for BRANCH in $(git branch --list | sed 's/\*//g'); do
+  COMMITS_AHEAD_OF_MASTER=$(git log $BITBUCKET_BRANCH..$BRANCH)
+  if [ -n "$COMMITS_AHEAD_OF_MASTER" ]; then
+    #echo "I am Ahead of my $BITBUCKET_BRANCH: $BRANCH ... now check behind"
+    COMMITS_BEHIND_MASTER=$(git log $BRANCH..$BITBUCKET_BRANCH)
+    if [ -n "$COMMITS_BEHIND_MASTER" ]; then
+      echo "Try to merge $BITBUCKET_BRANCH into $BRANCH"
+      git checkout $BRANCH
+      git merge --no-commit --no-ff $BITBUCKET_BRANCH
+      CONFLICTS=$(git ls-files -u | wc -l)
+      if [ "$CONFLICTS" -gt 0 ]; then
+        echo "There is a merge conflict. Aborting..."
+        # Git Merge abbrechen
+        git merge --abort
 
-               # Get UUID of Branch Author
-               export UUID_AUTHOR=$(curl https://api.bitbucket.org/2.0/repositories/${BITBUCKET_REPO_OWNER}/${BITBUCKET_REPO_SLUG}/refs/branches/$BRANCH \
-                -s -S \
-                -H 'Content-Type: application/json' \
-                -H "Authorization: Bearer ${BB_TOKEN}" | jq --raw-output '.target.author.user.uuid')
-               #echo $UUID_AUTHOR
+        ## Rate Limit prüfen
+        if [ $PR_RUN_LIMIT -gt -1 ] && [ $PR_COUNT -ge $PR_RUN_LIMIT ]; then
+          echo "Rate Limit Reached for this Run"
+        else
+          echo "Rate Limit = $PR_RUN_LIMIT not reached current PR Count = $PR_COUNT"
 
-               #PR Erstellen
-               curl https://api.bitbucket.org/2.0/repositories/${BITBUCKET_REPO_OWNER}/${BITBUCKET_REPO_SLUG}/pullrequests \
-                -s -S -X POST \
-                -H 'Content-Type: application/json' \
-                -H "Authorization: Bearer ${BB_TOKEN}" \
-                -d '{
-                    "title": "merge-resolver-bot/'$BRANCH'",
-                    "description": "Der Merge-Resolver Bot hat bei dem automatischen mergen von '$BITBUCKET_BRANCH' nach '$BRANCH' merge-conflicts erkannt. \n \n Bitte löse diese",
-                    "source": {
-                      "branch": {
-                        "name": "'$BITBUCKET_BRANCH'"
-                      }
-                    },
-                    "destination": {
-                      "branch": {
-                        "name": "'$BRANCH'"
-                      }
-                    },
-                    "close_source_branch": false,
-                    "reviewers": [{"uuid":"'$UUID_AUTHOR'"}]
-                  }'
+          export BB_TOKEN=$(curl -s -S -f -X POST -u "${BB_AUTH_STRING}" \
+            https://bitbucket.org/site/oauth2/access_token \
+            -d grant_type=client_credentials -d scopes="repository" | jq --raw-output '.access_token')
+          #echo "$BB_TOKEN"
 
-                  # For Testing only one PR
-                  # TODO: GGf. Rate Limit je Repo / global einbauen, damit nicht zu viele PR's pro tag / stunde gespammt werden.
-                  #exit 0
+          PR_TITLE="merge-resolver-bot/$BRANCH"
 
-            else
-               # No Merge Conflict Then commit and push
-               git commit -m "merge from $BITBUCKET_BRANCH"
-               git push
-            fi
+          # Check if PR is already OPEN
+          PRS_PAGE_SIZE=$(curl "https://api.bitbucket.org/2.0/repositories/${BITBUCKET_REPO_OWNER}/${BITBUCKET_REPO_SLUG}/pullrequests" \
+            -G -s -S \
+            --data-urlencode "fields=values.title,values.id,values.state,size,page,next" \
+            --data-urlencode 'q=(title="'$PR_TITLE'" AND state="OPEN")' \
+            -H 'Content-Type: application/json' \
+            -H "Authorization: Bearer ${BB_TOKEN}" | jq --raw-output '.size')
+
+          if [ $PRS_PAGE_SIZE -gt 0 ]; then
+            echo "PR $PR_TITLE already open, do nothing"
+          else
+            echo "PR $PR_TITLE not found, open a new PR"
+            # Get UUID of Branch Author
+            export UUID_AUTHOR=$(curl https://api.bitbucket.org/2.0/repositories/${BITBUCKET_REPO_OWNER}/${BITBUCKET_REPO_SLUG}/refs/branches/$BRANCH \
+              -s -S \
+              -H 'Content-Type: application/json' \
+              -H "Authorization: Bearer ${BB_TOKEN}" | jq --raw-output '.target.author.user.uuid')
+            #echo $UUID_AUTHOR
+
+            #PR Erstellen
+            curl https://api.bitbucket.org/2.0/repositories/${BITBUCKET_REPO_OWNER}/${BITBUCKET_REPO_SLUG}/pullrequests \
+              -s -S -X POST \
+              -H 'Content-Type: application/json' \
+              -H "Authorization: Bearer ${BB_TOKEN}" \
+              -d '{
+                  "title": "'$PR_TITLE'",
+                  "description": "Der Merge-Resolver Bot hat bei dem automatischen mergen von '$BITBUCKET_BRANCH' nach '$BRANCH' merge-conflicts erkannt. \n \n Bitte löse diese",
+                  "source": {
+                    "branch": {
+                      "name": "'$BITBUCKET_BRANCH'"
+                    }
+                  },
+                  "destination": {
+                    "branch": {
+                      "name": "'$BRANCH'"
+                    }
+                  },
+                  "close_source_branch": false,
+                  "reviewers": [{"uuid":"'$UUID_AUTHOR'"}]
+                }'
+            PR_COUNT=$((PR_COUNT + 1))
+          fi
         fi
+      else
+        # No Merge Conflict Then commit and push
+        git commit -m "auto-merge from $BITBUCKET_BRANCH"
+        git push
+      fi
     fi
+  fi
 done
